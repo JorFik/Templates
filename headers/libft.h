b2bff4e8d42cc18f6e96c2d864a745338c9d71d6
jfikents <jfikents@student.42Heilbron.de>
2023-11-21 11:02:21 +0100

    change structure of Libft for ease of use

--- headers/libft.h
+++ headers/libft.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 22:47:39 by JFikents          #+#    #+#             */
-/*   Updated: 2023/11/18 22:09:00 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/11/21 10:33:29 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,10 +66,10 @@ void			ft_putnbr_fd(int n, int fd);
 
 //BONUS
 t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **lst, t_list *new);
+void			ft_lstadd_front(t_list **lst, t_list *new_node);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_back(t_list **header, t_list *new);
+void			ft_lstadd_back(t_list **header, t_list *new_node);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **header, void (*del) (void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
