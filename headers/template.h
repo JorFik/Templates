3fc20323763da7f78e5a31ca5037510da99f8820
jfikents <jfikents@student.42Heilbron.de>
2023-12-18 22:58:16 +0100

    better template

--- headers/template.h
+++ headers/template.h
@@ -6,13 +6,28 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 12:15:17 by JFikents          #+#    #+#             */
-/*   Updated: 2023/11/21 12:19:40 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/12/18 22:57:15 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEMPLATE_H
 # define TEMPLATE_H
+
+// ** ---------------------------- LIBRARIES ---------------------------- ** //
+
 # include "libft.h"
+
+//_--------------------------------------------------------------------------_//
+
+// ** ------------------------------ MACROS ------------------------------ ** //
+
+# ifndef TEMPLATE_MACRO
+#  define TEMPLATE_MACRO	42
+# endif
+
+//_--------------------------------------------------------------------------_//
+
+// ** ------------------------- DATA STRUCTURES -------------------------- ** //
 
 typedef struct s_linked_list_template
 {
@@ -20,4 +35,33 @@ typedef struct s_linked_list_template
 	struct s_linked_list_template	*next;
 }	t_linked_list_template;
 
+//_--------------------------------------------------------------------------_//
+
+// ** ---------------------------- FUNCTIONS ---------------------------- ** //
+
+/**
+	@note//_DESCRIPTION
+	@brief ####
+	@brief
+	@note//_PARAMETERS
+	@param
+	@note//_RETURN
+	@return
+	@note//_NOTES
+	@note
+	@note//_SEE_ALSO
+	@see
+	@note//_WARNINGS
+	@warning
+ */
+void	ft_place_holder(void);
+
+//_--------------------------------------------------------------------------_//
+
 #endif
+/*
+// ?? ------------------------ ALLOWED FUNCTIONS ------------------------ ?? //
+
+
+//_--------------------------------------------------------------------------_//
+*/
